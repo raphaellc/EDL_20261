e1248d734d9662aadf71be38dd9ac39581e19d4f
Imevor Bleiddiaid Hellwood <duducnogueira@gmail.com>
2026-03-12 21:55:06 -0300

    Add files via upload

---
+++ Test/src/main/ClasseAmiga.java
@@ -0,0 +1,16 @@
+package main;
+
+public class ClasseAmiga {
+	private String nome;
+	
+	public ClasseAmiga(String nome) {
+		this.nome = nome;
+		
+	}
+	
+	public String getNome() {
+		return this.nome;
+		
+	}
+
+}
