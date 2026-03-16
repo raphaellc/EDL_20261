0c024dd1c708274c5daa0ebaef4594c6dcb971e0
DenianHoerlle <deniancloss@gmail.com>
2026-03-16 20:16:38 -0300

    added aula 1 exercicio 2 solution

---
+++ EDL/src/hoerlle/denian/aula1/Exercicio2.java
@@ -0,0 +1,51 @@
+package hoerlle.denian.aula1;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Produto churrasqueiraControleRemoto = new Produto(500, 100);
+
+        printProductInfo(churrasqueiraControleRemoto);
+
+        churrasqueiraControleRemoto.setEstoque(99);
+        churrasqueiraControleRemoto.setPreco(450);
+
+        printProductInfo(churrasqueiraControleRemoto);
+
+        churrasqueiraControleRemoto.setEstoque(-1);
+        churrasqueiraControleRemoto.setPreco(-1);
+
+    }
+
+    static void printProductInfo(Produto produto) {
+        System.out.println("Preço do produto: " + produto.getPreco());
+        System.out.println("Estoque do produto: " + produto.getEstoque());
+    }
+}
+
+class Produto {
+    private float preco;
+    private int estoque;
+
+    Produto(float preco, int estoque) {
+        this.preco = preco > 0 ? preco : 0;
+        this.estoque = estoque > 0 ? estoque : 0;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        if (preco < 0) System.out.println("Negativo não né, camarada?");
+        else this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        if (estoque < 0) System.out.println("Negativo não né, camarada?");
+        else this.estoque = estoque;
+    }
+}
