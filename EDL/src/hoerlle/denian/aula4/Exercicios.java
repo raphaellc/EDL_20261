e4705e94baf0ffdf4e62ad80e3a3499533164409
DenianHoerlle <deniancloss@gmail.com>
2026-03-22 15:21:01 -0300

    Added aula 4 remaining exercises

--- EDL/src/hoerlle/denian/aula4/Exercicios.java
+++ EDL/src/hoerlle/denian/aula4/Exercicios.java
@@ -1,4 +1,7 @@
-import java.io.FileNotFoundException;
+package hoerlle.denian.aula4;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercicios {
     static void main() {
@@ -11,8 +14,18 @@ public class Exercicios {
         // Exercício 4
 //        System.out.println(exerciseD(12345));
         // Exercicio 5
-        System.out.println(exerciseE(123));
-
+//        System.out.println(exerciseE(2468));
+        // Exercicio 6
+//        exerciseF(0, 10);
+        // Exercicio 7
+//        System.out.println(exerciseG(12, 24));
+        // Exercicio 8
+        List<Integer> numList = new ArrayList<>();
+        numList.add(5);
+        numList.add(10);
+        numList.add(20);
+        numList.add(0);
+        System.out.println(exerciseH(numList));
     }
 
     public static int exerciseA(int value, int counter) {
@@ -67,11 +80,43 @@ public class Exercicios {
             count *= 10;
         }
 
-        return 0;
+        int result = num - count * firstDigit;
 
-//        return (firstDigit + exerciseE(num - ));
+        return firstDigit + (exerciseE(result) * 10);
     }
 
+    public static void exerciseF(int start, int finish) {
+        // Escrever em ordem os valores inteiros de x a y, onde x e y são fornecidos como parâmetro.
+        // Obs.: A lista a ser escrita pode estar em ordem crescente ou decrescente;
 
+        System.out.print(start + (start == finish ? "." : ", "));
 
+        if (start < finish) exerciseF(start + 1, finish);
+        else if (start > finish) exerciseF( start - 1, finish);
+    }
+
+    public static int exerciseG(int num1, int num2) {
+        // Calcular o máximo divisor comum entre dois números inteiro e positivos, onde os dois
+        // número são fornecidos como parâmetro;
+
+        if (num1 == 0)
+            return num2;
+        return exerciseG(num2 % num1, num1);
+
+        // Não consegui fazer sozinho. Peguei daqui.
+        // https://www.geeksforgeeks.org/dsa/euclidean-algorithms-basic-and-extended/
+    }
+
+    public static int exerciseH(List<Integer> vector) {
+        // Achar maior elemento de um vetor
+
+        int first = vector.getFirst();
+        int size = vector.size();
+
+        if (size == 1) return first;
+
+        int nextNum = exerciseH(vector.subList(1, size));
+
+        return Math.max(first, nextNum);
+    }
 }
