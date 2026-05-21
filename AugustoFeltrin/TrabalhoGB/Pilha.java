0e2272d5ae07901985e64675b3f514d7552a5cf6
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2026-05-21 19:53:39 -0300

    fix pilha

--- AugustoFeltrin/TrabalhoGB/Pilha.java
+++ AugustoFeltrin/TrabalhoGB/Pilha.java
@@ -1,7 +1,6 @@
 package AugustoFeltrin.TrabalhoGB;
 
 public class Pilha<T> {
-
     private No<T> topo;
     private int tamanho;
 
@@ -11,37 +10,26 @@ public class Pilha<T> {
     }
 
     public void empilhar(T dado){
-
         No<T> novoNo = new No<T>(dado);
-
         novoNo.setProximo(topo);
-
         topo = novoNo;
-
         tamanho++;
     }
 
     public T desempilhar(){
-
         if(estaVazia()){
             throw new RuntimeException("Desempilhar: A pilha está vazia");
         }
-
         T dadoTemp = topo.getDado();
-
         topo = topo.getProximo();
-
         tamanho--;
-
         return dadoTemp;
     }
 
     public T getTopo(){
-
         if(estaVazia()){
             throw new RuntimeException("getTopo: A pilha está vazia");
         }
-
         return topo.getDado();
     }
 
@@ -54,17 +42,12 @@ public class Pilha<T> {
     }
 
     public void listarPilha(){
-
         if(estaVazia()){
             return;
         }
-
         No<T> noLista = topo;
-
         while(noLista != null){
-
             System.out.println(" - " + noLista.getDado());
-
             noLista = noLista.getProximo();
         }
     }
