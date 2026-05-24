23d40471e847bedd360d09c6644fdc9d48e24467
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2026-05-24 16:11:54 -0300

    feat mvp sistema atendimento

---
+++ AugustoFeltrin/TrabalhoGB/Main.java
@@ -0,0 +1,10 @@
+package AugustoFeltrin.TrabalhoGB;
+
+public class Main {
+    public static void main(String[] args){
+
+        SistemaAtendimento sistema = new SistemaAtendimento();
+
+        sistema.iniciarSimulacao();
+    }
+}
--- AugustoFeltrin/TrabalhoGB/SistemaAtendimento.java
+++ AugustoFeltrin/TrabalhoGB/SistemaAtendimento.java
@@ -1,3 +1,6 @@
+package AugustoFeltrin.TrabalhoGB;
+import java.util.Random;
+
 public class SistemaAtendimento {
     
     // Filas de espera
@@ -56,18 +59,121 @@ public class SistemaAtendimento {
         filaPrioritaria.enfileirar(senha);
     }
     
-    /*private*/ public Senha proximaSenha() {
-    	if(sequenciaNormais < 2) {
-    		if(!filaNormal.estaVazia()) {
-    			Senha senha = filaNormal.desenfileirar();
-    			sequenciaNormais++;
-    			return senha;
-    		} 
-    	} else if (!filaPrioritaria.estaVazia()) {
-    			Senha senha = filaPrioritaria.desenfileirar();
-    			sequenciaNormais = 0;
-    			return senha;
-    	}
-		return null;
+    private Senha proximaSenha(){
+
+    // tenta normal primeiro
+    if(sequenciaNormais < 2 && !filaNormal.estaVazia()){
+
+        sequenciaNormais++;
+
+        return filaNormal.desenfileirar();
     }
+
+    // tenta prioritária
+    if(!filaPrioritaria.estaVazia()){
+
+        sequenciaNormais = 0;
+
+        return filaPrioritaria.desenfileirar();
+    }
+
+    // fallback normal
+    if(!filaNormal.estaVazia()){
+
+        sequenciaNormais++;
+
+        return filaNormal.desenfileirar();
+    }
+        return null;
+    }
+
+    public void atenderProximaSenha(){
+        for(int i = 0; i < postos.length; i++){
+            Posto posto = postos[i];
+            if(posto.isAberto() && !posto.isOcupado()){
+                Senha senha = proximaSenha();
+
+                if(senha != null){
+                    posto.ocuparPosto(senha);
+                    historico.empilhar(senha);
+                    totalAtendidas++;
+                }
+            }
+        }
+    }
+
+    public void mostrarStatus(){
+        System.out.println("\n========== STATUS DO SISTEMA ==========");
+        
+        int totalFila = filaNormal.getTamanho() + filaPrioritaria.getTamanho();
+        System.out.println("Fila total: " + totalFila);
+
+        System.out.println("\nPróxima normal: " + filaNormal.frente());
+        System.out.println("Próxima prioritária: " + filaPrioritaria.frente());
+
+        System.out.println("\n--- POSTOS DE ATENDIMENTO ---");
+        for (int i = 0; i < postos.length; i++) {
+            System.out.println(postos[i]);
+        }
+
+        System.out.println("--- ESTATÍSTICAS ---");
+        System.out.println("Atendidas: " + totalAtendidas);
+        System.out.println("Desistências: " + totalDesistencias);
+        System.out.println("=======================================\n");
+    }
+
+    public void liberarPostos(){
+        for(int i = 0; i < postos.length; i++){
+            Posto posto = postos[i];
+            
+            if(posto.isAberto() && posto.isOcupado()){
+                posto.liberarPosto();
+            }
+        }
+    }
+    public void simularDesitencia(){
+        if(!filaNormal.estaVazia()){
+            Senha senha = filaNormal.desenfileirar();
+            totalDesistencias++;            
+            System.out.println("Senha " + senha + " desistiu");
+        } else if (!filaPrioritaria.estaVazia()){
+            Senha senha = filaPrioritaria.desenfileirar();
+            totalDesistencias++;
+            System.out.println("Senha " + senha + " desistiu");
+        }
+    }
+
+    public void mostrarHistorico(){
+        System.out.println("\n===== HISTÓRICO DE ATENDIMENTOS =====");
+        historico.listaPilha();
+    }
+
+    public void iniciarSimulacao(){
+        Random random = new Random();
+    
+        for(int i = 1; i <= 10; i++){
+            System.out.println("\n========= ITERAÇÃO " + i + " =========");
+            
+            int novaSenhas = random.nextInt(6) + 1;
+            int tipoSenha = random.nextInt(100);
+
+            for(int j = 0; j < novaSenhas; j++){
+                if(tipoSenha < 60){
+                    gerarSenhaNormal();
+                } else {
+                    gerarSenhaPrioritaria();
+                }
+            }
+        
+            if(random.nextInt(100) < 20){
+                simularDesitencia();
+            }
+            atenderProximaSenha();
+            mostrarStatus();
+            liberarPostos();
+        } 
+
+        mostrarHistorico();
+    }
+
 }
